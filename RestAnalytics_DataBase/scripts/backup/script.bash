0a0f95ec39567efd910c38d940d4ba2f56f667bd
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-07-05 15:26:27 -0300

    feat(backup.sql, backup_data_and_structure_only.sql, backup_scructure_only.sql, script.bash): Se agrego script.bash para realizar los backups de los tres tipos presentados en clase y scripts de SQL con un backup diferente.

---
+++ RestAnalytics_DataBase/scripts/backup/script.bash
@@ -0,0 +1,8 @@
+# 1. Dump Data and Structure (Respaldar Datos y Estructura):
+mysqldump -u root -p RestAnalytics > backup_data_and_structure.sql
+
+# 2. Dump Data Only (Respaldar solo Datos):
+mysqldump -u root -p --no-create-info RestAnalytics > backup.sql
+
+# 3. Dump Structure Only (Respaldar solo Estructura):
+mysqldump -u root -p --no-data RestAnalytics > backup_structure_only.sql
