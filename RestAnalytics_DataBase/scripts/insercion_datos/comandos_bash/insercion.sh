acf9914620266ba870fb0fcbfece5728b189ea9c
Cueva Tomas Santiago <tomassantiagocueva@gmail.com>
2023-07-18 16:53:57 -0300

    fix(environment):Dentro del entorno es posible hacer la ejecucaión de inserción por importación correctamente.

--- RestAnalytics_DataBase/scripts/insercion_datos/comandos_bash/insercion.sh
+++ RestAnalytics_DataBase/scripts/insercion_datos/comandos_bash/insercion.sh
@@ -1,33 +1,31 @@
-load data local infile 
-'C:\\Users\\tomas\\OneDrive\\Escritorio\\SQL\\ProyectoFinal\\DB\\restaurants.csv'
-into table restaurants
-fields terminated by ';'
-terminated by '\r\n'; 
+SET GLOBAL local_infile=1;
 
+LOAD DATA LOCAL INFILE '/home/app/csv/restaurants.csv'
+INTO TABLE restaurants
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
 
-load data local infile 
-'C:\\Users\\tomas\\OneDrive\\Escritorio\\SQL\\ProyectoFinal\\DB\\administratives.csv'
-into table administratives
-fields terminated by ';'
-terminated by '\r\n'; 
+LOAD DATA LOCAL INFILE '/home/app/csv/administratives.csv'
+INTO TABLE administratives
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
 
+LOAD DATA LOCAL INFILE '/home/app/csv/dishes.csv'
+INTO TABLE dishes
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
 
-load data local infile 
-'C:\\Users\\tomas\\OneDrive\\Escritorio\\SQL\\ProyectoFinal\\DB\\dishes.csv'
-into table dishes
-fields terminated by ';'
-terminated by '\r\n'; 
+LOAD DATA LOCAL INFILE '/home/app/csv/customers.csv'
+INTO TABLE customers
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
 
+LOAD DATA LOCAL INFILE '/home/app/csv/sales.csv'
+INTO TABLE sales
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
 
-load data local infile 
-'C:\\Users\\tomas\\OneDrive\\Escritorio\\SQL\\ProyectoFinal\\DB\\sales.csv'
-into table sales
-fields terminated by ';'
-terminated by '\r\n'; 
-
-
-load data local infile 
-'C:\\Users\\tomas\\OneDrive\\Escritorio\\SQL\\ProyectoFinal\\DB\\dishes_per_sale.csv'
-into table dishes_per_sale
-fields terminated by ';'
-terminated by '\r\n'; 
+LOAD DATA LOCAL INFILE '/home/app/csv/dishes_per_sale.csv'
+INTO TABLE dishes_per_sale
+FIELDS TERMINATED BY ';'
+IGNORE 1 LINES;
